321cf05d20e5d26a22a32dfaa114f393d1100937
hasithauom <88756920+hasithauom@users.noreply.github.com>
2023-12-14 00:23:59 +0530

    ft_hm_done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import { useState } from "react";
+
 const api = {
   key: "e8810468276983ffcf039ee331d5a392",
   base: "http://api.openweathermap.org/data/2.5",
 };
 
+
 function App() {
+
+  const { Search, setSearch } = useState("");
+
   return (
     <div>
       {/* header */}
@@ -12,13 +18,23 @@ function App() {
       </header>
 
       {/* search box */}
-      <input />
+      <div>
+        <input
+          type="text"
+          placeholder="search ..."
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </div>
 
       {/* location */}
+      {/* hard code for yet */}
+      <p>Sri lanka</p>
 
       {/* Temperature f/c */}
+      <p>32 f</p>
 
       {/* Condition */}
+      <p>Sunny</p>
     </div>
   );
 }
