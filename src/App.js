3451bad0152d4428d48a5abeb92405d972ad6943
hasithauom <88756920+hasithauom@users.noreply.github.com>
2023-12-15 23:09:38 +0530

    home_ft

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import "./App.css";
+// import "./App.css";
 import { useState } from "react";
+import WeatherHome from "./Component/WeatherHome";
+import NavBar from "./Component/NavBar";
 
 const api = {
   key: "7d4f151c7d97b3c9b10ce95280eac359",
@@ -19,49 +21,49 @@ function App() {
   };
 
   return (
-    <div className="w-screen">
-      <header className="App-header">
-        {/* HEADER  */}
-        <h1>Weather App</h1>
-
-        {/* Search Box  */}
-        <div>
-          <div class="relative w-full">
-            <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none w-1/3">
-              <svg
-                class="w-4 h-4 text-gray-500 dark:text-gray-400"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 20 20"
-              >
-                <path
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-                />
-              </svg>
-            </div>
-            <input
-              type="search"
-              id="default-search"
-              onChange={(e) => setSearch(e.target.value)}
-              class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="Search Mockups, Logos..."
-              required
-            />
-            <button
-              type="submit"
-              onClick={searchPressed}
-              class="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+    <div className="w-full h-full">
+      <div>
+        <NavBar />
+      </div>
+      {/* Search Box  */}
+      <div>
+        <div class="relative w-full">
+          <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none w-1/3">
+            <svg
+              class="w-4 h-4 text-gray-500 dark:text-gray-400"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 20 20"
             >
-              Search
-            </button>
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+              />
+            </svg>
           </div>
+          <input
+            type="search"
+            id="default-search"
+            onChange={(e) => setSearch(e.target.value)}
+            class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Search Mockups, Logos..."
+            required
+          />
+          <button
+            type="submit"
+            onClick={searchPressed}
+            class="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            Search
+          </button>
         </div>
+      </div>
 
+      <div>
         {typeof weather.main !== "undefined" ? (
           <div>
             {/* Location  */}
@@ -77,7 +79,11 @@ function App() {
         ) : (
           ""
         )}
-      </header>
+      </div>
+
+      <div className="">
+        <WeatherHome />
+      </div>
     </div>
   );
 }
---
+++ src/Component/WeatherHome.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function WeatherHome() {
+  return (
+    <div class="box-content h-[550px] w-4/6 border-4 bg-slate-400 md:box-content">
+      WeatherHome
+    </div>
+  );
+}
+
+export default WeatherHome;
